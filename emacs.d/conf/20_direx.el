a0b82b833c3db4334df3d36fa816a9dd378bb724
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2014-09-10 10:31:39 +0900

    add popwin and direx

---
+++ emacs.d/conf/20_direx.el
@@ -0,0 +1,8 @@
+;; popwin
+(require 'popwin)
+
+;; direx
+(require 'direx)
+(push '(direx:direx-mode :position left :width 40 :dedicated t)
+      popwin:special-display-config)
+(global-set-key (kbd "C-x C-j") 'direx:jump-to-directory-other-window)
