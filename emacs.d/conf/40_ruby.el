634391b17793a18684d05f296d2f6390d5e6a74f
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2014-10-21 23:52:19 +0900

    add config for ruby mode indent

--- emacs.d/conf/40_ruby.el
+++ emacs.d/conf/40_ruby.el
@@ -1,5 +1,21 @@
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 
+(setq ruby-deep-indent-paren-style nil)
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
+
 ;; robe
 (add-hook 'ruby-mode-hook
           (lambda ()
