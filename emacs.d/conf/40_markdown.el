22345ea7efe010ec4da5d11f7449246351688819
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2015-02-06 13:24:56 +0900

    use Marked 2 in order to preview markdown

--- emacs.d/conf/40_markdown.el
+++ emacs.d/conf/40_markdown.el
@@ -1,2 +1,12 @@
 (setq auto-mode-alist (cons '("\\.md$" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.markdown$" . markdown-mode) auto-mode-alist))
+
+;; markdown-preview-file
+(defun markdown-preview-file ()
+  "run Marked on the current file and revert the buffer"
+  (interactive)
+  (shell-command
+   (format "open -a /Applications/Marked.app %s"
+           (shell-quote-argument (buffer-file-name))))
+  )
+(global-set-key "\C-cm" 'markdown-preview-file)
