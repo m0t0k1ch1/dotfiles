c6cdc993b71a5ac0c5789c67737747d84266ebef
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2016-12-07 13:18:28 +0900

    emacs - update config for helm

--- emacs.d/conf/10_helm.el
+++ emacs.d/conf/10_helm.el
@@ -8,3 +8,4 @@
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
