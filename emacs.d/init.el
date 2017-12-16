3cb0687191dfc27dd685b6ff6c634250499a3528
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2017-12-16 20:18:41 +0900

    [emacs] do not use theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,11 +5,7 @@
 
 ;; load-path
 (add-to-list 'load-path "~/.emacs.d/elisp")
-(add-to-list 'load-path "~/.emacs.d/themes")
 (add-to-list 'load-path "~/.ghq/src/github.com/nsf/gocode/emacs")
-
-;; custom-theme-load-path
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
 ;; exec-path
 (add-to-list 'exec-path (expand-file-name "/usr/local/bin"))
@@ -18,26 +14,3 @@
 ;; init-loader
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/conf")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes nil)
- '(custom-safe-themes
-   (quote
-    ("8453c6ba2504874309bdfcda0a69236814cefb860a528eb978b5489422cb1791" "1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" "52588047a0fe3727e3cd8a90e76d7f078c9bd62c0b246324e557dfa5112e0d0c" "cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "5ee12d8250b0952deefc88814cf0672327d7ee70b16344372db9460e9a0e3ffc" default)))
- '(fci-rule-color "#2a2a2a")
- '(package-selected-packages
-   (quote
-    (helm solidity-mode dockerfile-mode go-mode auto-complete obsidian-theme yaml-mode web-mode toml-mode scala-mode2 ruby-block python-mode php-mode nginx-mode neotree markdown-mode lua-mode js2-mode jedi init-loader go-autocomplete git-gutter flycheck ensime cython-mode csharp-mode autopair auto-install auto-compile))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
