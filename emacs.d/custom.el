814c5a2c07db534e511e13ca1716f18bf30c0502
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-09-29 13:45:30 +0900

    [emacs] save custom variables as custom.el

---
+++ emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yaml-mode web-mode solidity-mode php-mode obsidian-theme nginx-mode neotree multiple-cursors markdown-mode json-mode js2-mode init-loader idle-highlight-mode helm go-eldoc go-autocomplete git-gutter flycheck expand-region exec-path-from-shell dockerfile-mode cask bind-key))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
