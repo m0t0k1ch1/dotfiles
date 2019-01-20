4a76d260b68879c502a0a2d9affdb40e6dd3924c
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2019-01-21 02:44:38 +0900

    [emacs] update neotree config

--- emacs.d/conf/10_neotree.el
+++ emacs.d/conf/10_neotree.el
@@ -1,7 +1,8 @@
 (setq neo-show-hidden-files t)
 (setq neo-smart-open t)
 
+(bind-key "C-u" 'neotree-select-up-node)
 (bind-key "C-c t" 'neotree-toggle)
 
-;; disable display-line-numbers-mode in neotree window
+;; disable display-line-numbers-mode in neotree buffer
 (add-hook 'neotree-mode-hook (lambda () (display-line-numbers-mode 0)))
