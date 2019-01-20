286849d48b1cff306d5e1ac865ffde8e8417780d
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2019-01-21 03:12:50 +0900

    [emacs] update helm config

--- emacs.d/conf/10_helm.el
+++ emacs.d/conf/10_helm.el
@@ -1,11 +1,9 @@
 (helm-mode)
 
-(bind-key "C-x b" 'helm-for-files)
+(bind-key "C-x m" 'helm-mini)
+(bind-key "C-x b" 'helm-buffers-list)
 (bind-key "C-x y" 'helm-show-kill-ring)
 (bind-key "C-x C-f" 'helm-find-files)
 (bind-key "C-x C-x" 'helm-M-x)
-(bind-key "M-x" 'helm-M-x)
 (bind-key "C-h" 'delete-backward-char helm-map)
 (bind-key "C-h" 'delete-backward-char helm-find-files-map)
-(bind-key "TAB" 'helm-execute-persistent-action helm-find-files-map)
-(bind-key "TAB" 'helm-execute-persistent-action helm-read-file-map)
