b2b0b5ef6d8d59cb8a8841922ab6ac1ec2e0aa7b
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2019-01-21 00:11:28 +0900

    [emacs] disalbe display-line-numbers-mode in neotree window

--- emacs.d/conf/10_neotree.el
+++ emacs.d/conf/10_neotree.el
@@ -1,3 +1,7 @@
 (setq neo-show-hidden-files t)
 (setq neo-smart-open t)
+
 (bind-key "C-c t" 'neotree-toggle)
+
+;; disable display-line-numbers-mode in neotree window
+(add-hook 'neotree-mode-hook (lambda () (display-line-numbers-mode 0)))
